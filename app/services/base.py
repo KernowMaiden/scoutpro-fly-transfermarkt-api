1d50671127369491b154b21451f83766c3e6c288
KernowMaiden <106260276+KernowMaiden@users.noreply.github.com>
2025-10-24 18:01:02 +0100

    Update base.py

--- app/services/base.py
+++ app/services/base.py
@@ -16,12 +16,6 @@ from app.utils.xpath import Pagination
 class TransfermarktBase:
     """
     Base class for making HTTP requests to Transfermarkt and extracting data from the web pages.
-
-    Args:
-        URL (str): The URL for the web page to be fetched.
-    Attributes:
-        page (ElementTree): The parsed web page content.
-        response (dict): A dictionary to store the response data.
     """
 
     URL: str
@@ -31,37 +25,29 @@ class TransfermarktBase:
     def make_request(self, url: Optional[str] = None) -> Response:
         """
         Make an HTTP GET request to the specified URL.
-
-        Args:
-            url (str, optional): The URL to make the request to. If not provided, the class's URL
-                attribute will be used.
-
-        Returns:
-            Response: An HTTP Response object containing the server's response to the request.
-
-        Raises:
-            HTTPException: If there are too many redirects, or if the server returns a client or
-                server error status code.
         """
         url = self.URL if not url else url
         try:
-            response: Response = requests.get(
-                url=url,
-                headers={
-                    "User-Agent": (
-                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                        "AppleWebKit/537.36 (KHTML, like Gecko) "
-                        "Chrome/113.0.0.0 "
-                        "Safari/537.36"
-                    ),
-                },
-            )
+            headers = {
+                "User-Agent": (
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                    "AppleWebKit/605.1.15 (KHTML, like Gecko) "
+                    "Version/15.5 Safari/605.1.15"
+                ),
+                "Accept-Language": "en-US,en;q=0.9",
+                "Referer": "https://www.google.com/",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Connection": "keep-alive"
+            }
+
+            response: Response = requests.get(url=url, headers=headers, timeout=15)
         except TooManyRedirects:
             raise HTTPException(status_code=404, detail=f"Not found for url: {url}")
         except ConnectionError:
             raise HTTPException(status_code=500, detail=f"Connection error for url: {url}")
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Error for url: {url}. {e}")
+
         if 400 <= response.status_code < 500:
             raise HTTPException(
                 status_code=response.status_code,
@@ -75,73 +61,22 @@ class TransfermarktBase:
         return response
 
     def request_url_bsoup(self) -> BeautifulSoup:
-        """
-        Fetch the web page content and parse it using BeautifulSoup.
-
-        Returns:
-            BeautifulSoup: A BeautifulSoup object representing the parsed web page content.
-
-        Raises:
-            HTTPException: If there are too many redirects, or if the server returns a client or
-                server error status code.
-        """
         response: Response = self.make_request()
         return BeautifulSoup(markup=response.content, features="html.parser")
 
     @staticmethod
     def convert_bsoup_to_page(bsoup: BeautifulSoup) -> ElementTree:
-        """
-        Convert a BeautifulSoup object to an ElementTree.
-
-        Args:
-            bsoup (BeautifulSoup): The BeautifulSoup object representing the parsed web page content.
-
-        Returns:
-            ElementTree: An ElementTree representing the parsed web page content for further processing.
-        """
         return etree.HTML(str(bsoup))
 
     def request_url_page(self) -> ElementTree:
-        """
-        Fetch the web page content, parse it using BeautifulSoup, and convert it to an ElementTree.
-
-        Returns:
-            ElementTree: An ElementTree representing the parsed web page content for further
-                processing.
-
-        Raises:
-            HTTPException: If there are too many redirects, or if the server returns a client or
-                server error status code.
-        """
         bsoup: BeautifulSoup = self.request_url_bsoup()
         return self.convert_bsoup_to_page(bsoup=bsoup)
 
     def raise_exception_if_not_found(self, xpath: str):
-        """
-        Raise an exception if the specified XPath does not yield any results on the web page.
-
-        Args:
-            xpath (str): The XPath expression to query elements on the page.
-
-        Raises:
-            HTTPException: If the specified XPath query does not yield any results, indicating an invalid request.
-        """
         if not self.get_text_by_xpath(xpath):
             raise HTTPException(status_code=404, detail=f"Invalid request (url: {self.URL})")
 
     def get_list_by_xpath(self, xpath: str, remove_empty: Optional[bool] = True) -> Optional[list]:
-        """
-        Extract a list of elements from the web page using the specified XPath expression.
-
-        Args:
-            xpath (str): The XPath expression to query elements on the page.
-            remove_empty (bool, optional): If True, remove empty or whitespace-only elements from
-                the list. Default is True.
-
-        Returns:
-            Optional[list]: A list of elements extracted from the web page based on the XPath query.
-                If remove_empty is True, empty or whitespace-only elements are filtered out.
-        """
         elements: list = self.page.xpath(xpath)
         if remove_empty:
             elements_valid: list = [trim(e) for e in elements if trim(e)]
@@ -158,25 +93,6 @@ class TransfermarktBase:
         iloc_to: Optional[int] = None,
         join_str: Optional[str] = None,
     ) -> Optional[str]:
-        """
-        Extract text content from the web page using the specified XPath expression.
-
-        Args:
-            xpath (str): The XPath expression to query elements on the page.
-            pos (int, optional): Index of the element to extract if multiple elements match the
-                XPath. Default is 0.
-            iloc (int, optional): Extract a single element by index, used as an alternative to 'pos'.
-            iloc_from (int, optional): Extract a range of elements starting from the specified
-                index (inclusive).
-            iloc_to (int, optional): Extract a range of elements up to the specified
-                index (exclusive).
-            join_str (str, optional): If provided, join multiple text elements into a single string
-                using this separator.
-
-        Returns:
-            Optional[str]: The extracted text content from the web page based on the XPath query and
-                optional parameters. If no matching element is found, None is returned.
-        """
         element = self.page.xpath(xpath)
 
         if not element:
@@ -206,16 +122,6 @@ class TransfermarktBase:
             return None
 
     def get_last_page_number(self, xpath_base: str = "") -> int:
-        """
-        Retrieve the last page number for a paginated result based on the provided base XPath.
-
-        Args:
-            xpath_base (str): The base XPath for extracting page number information.
-
-        Returns:
-            int: The last page number for search results. Returns 1 if no page numbers are found.
-        """
-
         for xpath in [Pagination.PAGE_NUMBER_LAST, Pagination.PAGE_NUMBER_ACTIVE]:
             url_page = self.get_text_by_xpath(xpath_base + xpath)
             if url_page:
